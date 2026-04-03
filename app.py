2d7326b215198170f950dd85ce50e50c5f7c9d58
Utpal Jani <utpaljani510@gmail.com>
2026-04-03 13:38:30 +0530

    Cleaning up profile: moved project to SkinBuzz

--- app.py
+++
@@ -1,7 +0,0 @@
-from app import create_app,db
-from app.models import User,Scan
-app=create_app()
-if __name__=="__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
--- app/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .upload_route import upload
-from .auth_route import auth
-def register_routes(app):
-    app.register_blueprint(upload, url_prefix="/")
-    app.register_blueprint(auth)
